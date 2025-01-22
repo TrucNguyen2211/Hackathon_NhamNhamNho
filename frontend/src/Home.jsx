0f9ca05a41bf1d3bf52fee4c8b84414955f1af18
KuuuHan <s3986602@rmit.edu.vn>
2025-01-22 16:26:20 +0700

    Update Calendar

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom";
 import { Navbar } from "./component/Navbar";
+import Calendar from "./component/Calendar";
 
 function Home() {
     return (
-        <>
+        <div id ="root">
             <header className="header">
                 <Navbar />
             </header>
+
             <div className="content">
                 <h1>Home</h1>
                 <p>Welcome to the Home page!</p>
             </div>
-        </>
+
+            <div className="calendar-box">
+                 <Calendar />
+            </div>
+        </div>
     );
 }
 
