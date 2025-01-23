902c45f910812d09d8059e0c9066bc04353a2eab
KuuuHan <s3986602@rmit.edu.vn>
2025-01-23 09:41:43 +0700

    Finish home with a crousel

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,25 +2,14 @@ import { Link } from "react-router-dom";
 import { Navbar } from "./component/Navbar";
 import { Outlet } from "react-router-dom";
 import Calendar from "./component/Calendar";
+import HomeCarousel from "./component/HomeCarousel";
 
 function Home() {
     return (
         <div id ="root">
-         
-
-            <div className="content">
-                <h1>Home</h1>
-                <p>Welcome to the Home page!</p>
+            <div className="carousel">
+                <HomeCarousel />
             </div>
-
-            <p>Welcome to the Home page!</p>
-            <p>Welcome to the Home page!</p>
-            <p>Welcome to the Home page!</p>
-            <p>Welcome to the Home page!</p>
-            <p>Welcome to the Home page!</p>
-            <p>Welcome to the Home page!</p>
-            <p>Welcome to the Home page!</p>
-            <p>Welcome to the Home page!</p>
 
             <div id="calendar-box" className="calendar-box">
                  <Calendar />
